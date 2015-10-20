bf96eae90ecbb733d5e242275e4979c0fe7741a2
Marco Scannadinari <m@scannadinari.co.uk>
2015-10-20 23:02:43 +0100

    add error checking to rethink server

--- rethinkserver.py
+++ rethinkserver.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
 
 import subprocess as sp
+import time
+import sys
 
 DRIVER_PORT = 8193
 ADMIN_PORT = 8194
 
 if __name__ == "__main__":
 	print("launching rethinkdb...")
-	status = sp.call(["rethinkdb", "--http-port", str(ADMIN_PORT), "--driver-port", str(DRIVER_PORT)])
+	process = sp.Popen(["rethinkdb", "--http-port", str(ADMIN_PORT), "--driver-port", str(DRIVER_PORT)])
+
+	try:
+		while True:
+			time.sleep(1)
+			if process.poll():
+				sys.exit(1)
+	except KeyboardInterrupt:
+		sys.exit(1)

b19083915b3cdce4641b0a6b7e6dcbd7690e2000
Marco Scannadinari <m@scannadinari.co.uk>
2015-10-20 18:53:34 +0100

    add rethinkdb execution code

--- rethinkserver.py
+++ rethinkserver.py
@@ -1,4 +1,10 @@
-import subprocess
+#!/usr/bin/env python3
+
+import subprocess as sp
 
 DRIVER_PORT = 8193
 ADMIN_PORT = 8194
+
+if __name__ == "__main__":
+	print("launching rethinkdb...")
+	status = sp.call(["rethinkdb", "--http-port", str(ADMIN_PORT), "--driver-port", str(DRIVER_PORT)])

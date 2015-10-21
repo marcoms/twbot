b7170913ea1dbcfa4742bffa176d3dc214785b47
Marco Scannadinari <m@scannadinari.co.uk>
2015-10-21 11:13:07 +0100

    twbot: connect to rethinkdb

--- twbot.py
+++ twbot.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+import sys
+import rethinkdb as r
+import bottle as b
+
+import rethinkserver
+
+try:
+	conn = r.connect(db="twbot", port=rethinkserver.DRIVER_PORT)
+except r.errors.ReqlDriverError:
+	print("could not connect to rethinkdb server - please launch rethinkserver.py and try again")
+	sys.exit(1)
